cd971bcfa9faa14710c72a180a4258c0bcb2c7ef
fevin1414 <fevinbiju1414@gmail.com>
2023-09-18 15:35:45 -0500

    setting up the starter files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJMEiZcOjHJcKqsqelqL3C3ybtn6ObN6I",
+  authDomain: "netflix-clone-558e2.firebaseapp.com",
+  projectId: "netflix-clone-558e2",
+  storageBucket: "netflix-clone-558e2.appspot.com",
+  messagingSenderId: "53860867772",
+  appId: "1:53860867772:web:608779ba4b6a255269bab9"
+};
