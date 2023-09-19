222b3403829cbc31ad960f5919a4f2cf19ed1dd2
fevin1414 <fevinbiju1414@gmail.com>
2023-09-19 08:24:58 -0500

    added scroll effect

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Nav.css"
 function Nav() {
+  const  [show,handleShow]=useState(false);
+  const transitionNavbar=()=>{
+  if(window.scrollY >100){
+    handleShow(true);
+
+  }else{
+    handleShow(false);
+
+  }
+  }
+
+ useEffect(()=>{
+  window.addEventListener("scroll",transitionNavbar)
+  return ()=>window.removeEventListener("scroll",transitionNavbar);
+ },[]);
   return (
-    <div className="nav nav_black">
+    <div className={`nav ${show && `nav_black`}`}>
   <div classNAme="nav_contents">
   <img
       className="nav_logo"
