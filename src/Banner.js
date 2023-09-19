9f34c61a373a5f6cfd8a9adc7db3567f32bb1633
fevin1414 <fevinbiju1414@gmail.com>
2023-09-19 09:09:34 -0500

    added banner elements

--- src/Banner.js
+++ src/Banner.js
@@ -8,9 +8,16 @@ function Banner() {
     backgroundImage:`url('https://images.thedirect.com/media/article_full/netflix-one-piece.jpg?imgeng=cmpr_75/')`
     }}>
 <div className='banner_contents'>
-<div className='banner_title'>
+<h1 className='banner_title'>Movie Name</h1>
+<div className='banner_button'>
+  <button className='banner_button'>Play</button>
+  <button className='banner_button'>My List</button>
+
 </div>
+<h1 className='banner_description'>This is a test description</h1>
+
 </div>
+<div className='banner--fadeBottom'/>
     </header>
   )
 }
