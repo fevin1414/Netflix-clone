ff15c888d9504cae0061436b33fbd0e074ee2ced
fevin1414 <fevinbiju1414@gmail.com>
2023-09-19 09:29:14 -0500

    added more styling to homepage

--- src/Banner.js
+++ src/Banner.js
@@ -1,25 +1,26 @@
-import React from 'react'
-import "./Banner.css"
+import React from "react";
+import "./Banner.css";
 function Banner() {
   return (
-    <header className="banner" style={{
-      backgroundSize:"cover",
-    backgroundPosition:"center center",
-    backgroundImage:`url('https://images.thedirect.com/media/article_full/netflix-one-piece.jpg?imgeng=cmpr_75/')`
-    }}>
-<div className='banner_contents'>
-<h1 className='banner_title'>Movie Name</h1>
-<div className='banner_button'>
-  <button className='banner_button'>Play</button>
-  <button className='banner_button'>My List</button>
-
-</div>
-<h1 className='banner_description'>This is a test description</h1>
-
-</div>
-<div className='banner--fadeBottom'/>
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">This is a test description</h1>
+      </div>
+      <div className="banner--fadeBottom" />
     </header>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
