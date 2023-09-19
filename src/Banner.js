752e479e8b49094b3c328ac48e7749fd3e69db69
fevin1414 <fevinbiju1414@gmail.com>
2023-09-19 09:37:12 -0500

    added truncate and more styles

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Banner.css";
 function Banner() {
+  function truncate(string,n){
+    return string?.length > n ? string.substr(0,n-1)+'...':string;
+  }
   return (
     <header
       className="banner"
@@ -16,7 +19,7 @@ function Banner() {
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">This is a test description</h1>
+        <h1 className="banner_description">{truncate(`this is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`,150)}</h1>
       </div>
       <div className="banner--fadeBottom" />
     </header>
