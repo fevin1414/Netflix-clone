1345ae1b6c168d79345eee72935bfb956c974b62
fevin1414 <fevinbiju1414@gmail.com>
2023-09-19 13:02:45 -0500

    added trailer video on hovering

--- src/Banner.js
+++ src/Banner.js
@@ -2,17 +2,24 @@ import React, { useEffect, useState } from "react";
 import "./Banner.css";
 import axios from "./axios";
 import request from "./request";
+import { YouTubeAPIKey } from "./config";
+import YouTube from 'react-youtube';
 
 function Banner() {
   const [movie, setMovie] = useState(null);
+  const [isHovered, setIsHovered] = useState(false);
+  const [videoId, setVideoId] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await axios.get(request.fetchNetflixOriginals);
-        const randomIndex = Math.floor(Math.random() * response.data.results.length-1);
+        const randomIndex = Math.floor(Math.random() * response.data.results.length - 1);
         const randomMovie = response.data.results[randomIndex];
         setMovie(randomMovie);
+
+        const youtubeVideoId = await fetchYouTubeVideoId(randomMovie.name);
+        setVideoId(youtubeVideoId);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -25,39 +32,80 @@ function Banner() {
     console.log("Fetched movie:", movie);
   }, [movie]);
 
+  async function fetchYouTubeVideoId(movieName) {
+    try {
+      const API_KEY = YouTubeAPIKey;
+      const response = await axios.get(`https://www.googleapis.com/youtube/v3/search`, {
+        params: {
+          key: API_KEY,
+          q: `${movieName} trailer`,
+          type: 'video',
+          part: 'id',
+          maxResults: 1,
+        },
+      });
+
+      if (response.data.items.length > 0) {
+        return response.data.items[0].id.videoId;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error("Error fetching YouTube video ID:", error);
+      return null;
+    }
+  }
+
+  const opts = {
+    width: '100%',
+
+    playerVars: {
+      autoplay: 1,
+      controls: 0,
+    },
+  };
+
+  const onReady = (event) => {
+    if (event.target && event.target.playVideo) {
+      event.target.playVideo();
+    }
+  };
+
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
 
-  if (!movie) {
-
-    return null;
-  }
-
   return (
-    <header
+    <div
       className="banner"
-      style={{
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-        backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
-      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="banner_contents">
-        <h1 className="banner_title">Movie Name</h1>
-        <div className="banner_buttons">
-          <button className="banner_button">Play</button>
-          <button className="banner_button">My List</button>
+      {isHovered && videoId ? (
+        <div className="banner_video">
+          <YouTube videoId={videoId} opts={opts} onReady={onReady} />
         </div>
-        <h1 className="banner_description">
-          {truncate(
-            `this is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`,
-            150
-          )}
-        </h1>
-      </div>
+      ) : (
+        <div>
+          <img
+            className="banner_image"
+            src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
+            alt={movie?.name}
+          />
+          <div className="banner_contents">
+            <h1 className="banner_title">{movie?.name}</h1>
+            <div className="banner_buttons">
+              <button className="banner_button">Play</button>
+              <button className="banner_button">My List</button>
+            </div>
+            <h1 className="banner_description">
+              {truncate(movie?.overview, 150)}
+            </h1>
+          </div>
+        </div>
+      )}
       <div className="banner--fadeBottom" />
-    </header>
+    </div>
   );
 }
 
