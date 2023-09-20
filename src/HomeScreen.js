25af304e377682f10ae7f2bf40ed8239ccbfa943
fevin1414 <fevinbiju1414@gmail.com>
2023-09-20 13:19:30 -0500

    added row

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,15 +1,57 @@
-import React from 'react'
-import "./homeScreen.css"
-import Nav from './Nav'
-import Banner from './Banner'
+import React from "react";
+import "./homeScreen.css";
+import Nav from "./Nav";
+import Row from "./Row"
+import Banner from "./Banner";
+import request from "./request";
 function HomeScreen() {
   return (
     <div className="homeScreen">
-<Nav />
-<Banner/>
+      <Nav />
+      <Banner />
+      <Row
+        title="Netflix Originals"
+        fetchurl={request.fetchNetflixOriginals}
+        isLargeRow
+      />
 
+      <Row
+        title="Trending Now"
+        fetchurl={request.fetchNetflixTrending}
+        isLargeRow
+      />
+      <Row
+        title="Top Rated"
+        fetchurl={request.fetchNetflixTopRated}
+        isLargeRow
+      />
+      <Row
+        title="Action Movies"
+        fetchurl={request.fetchActionMovies}
+        isLargeRow
+      />
+      <Row
+        title="Comedy Movies"
+        fetchurl={request.fetchComedyMovies}
+        isLargeRow
+      />
+      <Row
+        title="Horror Movies"
+        fetchurl={request.fetchHorrorMovies}
+        isLargeRow
+      />
+      <Row
+        title="Rommance Movies"
+        fetchurl={request.fetchRomanceMovies}
+        isLargeRow
+      />
+      <Row
+        title="Documentaries Movies"
+        fetchurl={request.fetchDocumentaries}
+        isLargeRow
+      />
     </div>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
