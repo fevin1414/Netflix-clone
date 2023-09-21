84d903d5d90f441cdd4d9b510fd3cbe21f777e69
fevin1414 <fevinbiju1414@gmail.com>
2023-09-21 16:09:49 -0500

    added sign In screen

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase'
 const firebaseConfig = {
   apiKey: "AIzaSyAJMEiZcOjHJcKqsqelqL3C3ybtn6ObN6I",
   authDomain: "netflix-clone-558e2.firebaseapp.com",
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import "./SignupScreen.css"
+import React from "react";
+import "./SignupScreen.css";
 function SignupScreen() {
+  const register=(e)=>{
+    e.preventDefault();
+  }
+  const signIn=(e)=>{
+    e.preventDefault();
+  }
   return (
     <div className="signupScreen">
-
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email Address" />
+        <input type="password" placeholder="password" />
+       <button type="submit" onClick={signIn}>Sign In</button>
+       <h4>
+        <span className="gray">New to Netflix?</span><span className="link" onClick={register}> sign Up now</span></h4>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignupScreen
+export default SignupScreen;
